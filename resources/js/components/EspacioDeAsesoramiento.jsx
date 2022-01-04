1a795a1234e37285c50c5d8b4de4ec13e25cf383
Carlos <elfijocl@gmail.com>
2022-01-04 19:36:44 -0400

    EstilosBotones

--- resources/js/components/EspacioDeAsesoramiento.jsx
+++ resources/js/components/EspacioDeAsesoramiento.jsx
@@ -30,7 +30,7 @@ const EspacioDeAsesoramiento = () => {
               <div className="nav nav-tabs" id="nav-tab" role="tablist">
                 <a className="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Grupo Empresa</a>
                 <a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Calendario</a>
-                <a className="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">WorkSpace</a>
+                <a className="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Portafolio</a>
               </div>
             </nav>
             <div id="contenido-GE" className="tab-content  d-flex justify-content-center" id="nav-tabContent">
