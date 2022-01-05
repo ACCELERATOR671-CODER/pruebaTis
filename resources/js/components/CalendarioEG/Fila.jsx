5f6ef1c3616741d2ba0d93335c8df258151e2c78
Cesar <kprojet671@gmail.com>
2022-01-05 00:19:03 -0400

    prueba we

--- resources/js/components/CalendarioEG/Fila.jsx
+++ resources/js/components/CalendarioEG/Fila.jsx
@@ -77,11 +77,10 @@ const Fila = ({cambio, setCambio, data, index, rol}) => {
                         </ul>
                     </TD>
                    <TD >
-                       <ul>
                         <div className='d-flex justify-content-end'>
-                            <button onClick={eliminarFila}>
+                            {(rol == 'Consultor') && <button onClick={eliminarFila}>
                                 <Trash icon={ faTrashAlt }/>
-                            </button>
+                            </button>}
                         </div>
                         <div>
                             { actividades[2].map((obj) => <li>{obj.nombreOpcion}</li> ) }
