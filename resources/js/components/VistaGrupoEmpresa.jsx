ad9ffe6d3ad12a0153c60a8e2171a3d4d1a32cfc
Jhilm <jnukuravuisente28@gmail.com>
2022-01-07 05:29:32 +0100

    con fecha de adrian

--- resources/js/components/VistaGrupoEmpresa.jsx
+++ resources/js/components/VistaGrupoEmpresa.jsx
@@ -14,33 +14,33 @@ const VistaGrupoEmpresa = () => {
     const [fechaLimite, setFechaLimite] = useState(null);
 
     const convertirDig = (n) => {
-      return (n < 10) ? '0'+n : n;
+        return (n < 10) ? '0' + n : n;
     }
 
-    useEffect(()=>{
+    useEffect(() => {
 
-      const data = new FormData();
-      data.append('tipoOpcion','Actividad');
-      data.append('nombreOpcion','Entrega de propuestas');
+        const data = new FormData();
+        data.append('tipoOpcion', 'Actividad');
+        data.append('nombreOpcion', 'Entrega de propuestas');
 
-      fetch('api/obtenerEventoGeneral',{
-        method: 'POST',
-        body: data,
-      })
-      .then((res) => res.json())
-      .then((datos) => {
-        if (datos.idEvento) {
-          setFechaLimite(datos.fecha_final);
-        }
-      });
+        fetch('api/obtenerEventoGeneral', {
+            method: 'POST',
+            body: data,
+        })
+            .then((res) => res.json())
+            .then((datos) => {
+                if (datos.idEvento) {
+                    setFechaLimite(datos.fecha_final);
+                }
+            });
 
-      const date = new Date();
-      const [dia, mes, anio] = [convertirDig(date.getDate()),
-                                convertirDig(date.getMonth()+1),
-                                convertirDig(date.getFullYear())];
+        const date = new Date();
+        const [dia, mes, anio] = [convertirDig(date.getDate()),
+        convertirDig(date.getMonth() + 1),
+        convertirDig(date.getFullYear())];
 
-      setFechaAct(`${anio}-${mes}-${dia}`);
-    },[])
+        setFechaAct(`${anio}-${mes}-${dia}`);
+    }, [])
 
     const idUser = sessionStorage.getItem('id');
 
@@ -75,36 +75,20 @@ const VistaGrupoEmpresa = () => {
             });
     };
 
-    const [valido, setValido] = useState('false');
+
     const [activo, setActivo] = useState('true');
 
     useEffect(() => {
-        const dataA = new FormData();
-        dataA.append('id', sessionStorage.getItem('id'));
-        dataA.append('ge', datos.nombre);
-        fetch('api/puedeValidarGE', {
-            method: 'POST',
-            body: dataA
-        })
-            .then((response) => {
-                if (response.status == 201) {
-                    setValido('true');
-                } else {
-                    /* setValido('false'); */
-                    setValido('true');
-                }
-            });
-
-        const dataF = new FormData();
-        dataF.append('id', sessionStorage.getItem('id'));
+        const data = new FormData();
+        data.append('id', sessionStorage.getItem('id'));
         fetch('api/establecerBotonValidar', {
             method: 'POST',
-            body: dataF
+            body: data
         })
             .then((response) => {
                 if (response.status == 201) {
                     setActivo('true');
-                }else{
+                } else {
                     setActivo('false');
                 }
             });
@@ -114,41 +98,42 @@ const VistaGrupoEmpresa = () => {
 
 
     return (
-            <Card>
-                <div className='formStyle'>
-                    <Titulo>{datos.nombre} </Titulo>
-                    {
-                        (idUser == datos.duenio) && (
-                           <BtmEdit onClick={redir}>
-                                <IconoEditar/>
-                           </BtmEdit>
-                        )
-                    }
+        <Card>
+            <div className='formStyle'>
+                <Titulo>{datos.nombre} </Titulo>
+                {
+                    (idUser == datos.duenio) && (
+                        <BtmEdit onClick={redir}>
+                            <IconoEditar />
+                        </BtmEdit>
+                    )
+                }
 
-                    <ContenedorDatos className=' mb-5'>
-                        <div className=' d-block text-left'>
-                            <label><strong>Nombre Abreviado: </strong>{datos.nombreAb}</label><br/>
-                            <label><strong>Telefono: </strong>{datos.telefono}</label><br/>
-                            <label><strong>Direccion: </strong>{datos.direccion}</label><br/>
-                            <label><strong>Organizacion Juridica: </strong>{datos.orgJur}</label><br/>
-                            <label><strong>Correo: </strong>{datos.email}</label><br/>
-                            <label><strong>Descripcion: </strong>{datos.descripcion}</label><br/>
-                        </div>
-                        <div>
-                            <img id='imagenGER' src = {'resources/'+datos.logo}/>
-                            {
-                                (fechaLimite) && (fechaAct) && (fechaAct <= fechaLimite) &&
-                                (<BotonSolicitarIngreso />)
-                            }
-                        </div>
-                    </ContenedorDatos>
-                </div>
-                <>
-                    {(valido == 'true') && (<><BotonSolicitud  onClick={onClick} valido={ activo } >
+                <ContenedorDatos className=' mb-5'>
+                    <div className=' d-block text-left'>
+                        <label><strong>Nombre Abreviado: </strong>{datos.nombreAb}</label><br />
+                        <label><strong>Telefono: </strong>{datos.telefono}</label><br />
+                        <label><strong>Direccion: </strong>{datos.direccion}</label><br />
+                        <label><strong>Organizacion Juridica: </strong>{datos.orgJur}</label><br />
+                        <label><strong>Correo: </strong>{datos.email}</label><br />
+                        <label><strong>Descripcion: </strong>{datos.descripcion}</label><br />
+                    </div>
+                    <div>
+                        <img id='imagenGER' src={'resources/' + datos.logo} />
+                        {
+                            (fechaLimite) && (fechaAct) && (fechaAct <= fechaLimite) &&
+                            (<BotonSolicitarIngreso />)
+                        }
+                    </div>
+                </ContenedorDatos>
+            </div>
+            <>
+                {(fechaLimite) && (fechaAct) && (fechaAct <= fechaLimite) &&
+                    (<><BotonSolicitud onClick={onClick} valido={activo} >
                         {(activo == 'true') ? "Validar Grupo Empresa" : "Eliminar Validacion"}
                     </BotonSolicitud ></>)}
-                </>
-            </Card>
+            </>
+        </Card>
     )
 }
 
